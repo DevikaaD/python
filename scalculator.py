1d67c81454c9b4eb233b90eef65f85f876ac91df
Devikaa-D <chn19cs043@ceconline.edu>
2022-08-06 00:05:03 +0530

    Simple calculator

---
+++ scalculator.py
@@ -0,0 +1,23 @@
+# Simple calculator
+
+print("enter the numbers")
+a = int(input("First number:"))
+b = int(input("Second number"))
+
+print("\n5The operations are:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+operation = input("\nSelect the operation:")
+
+if operation == '1':
+    print("The sum is:", str(a + b))
+elif operation=='2':
+    print("The sub is:", str(a-b))
+elif operation=='3':
+    print("The product is:", str(a*b))
+elif operation=='4':
+    print("The result is:", str(a/b))
+else:
+    print("Invalid operation")
