0d5f5c34f0ca5eaffa0725588119d3f6ddad8958
Devikaa-D <chn19cs043@ceconline.edu>
2022-08-22 19:41:20 +0530

    python

---
+++ Largest.py
@@ -0,0 +1,7 @@
+def LargestNumber(num1,num2):
+    if num1>num2:
+        return num1
+    else:
+        return num2
+
+largest=print('the largest number is:',LargestNumber(100,90))
---
+++ common_no_in_list.py
@@ -0,0 +1,5 @@
+
+
+list_a =[1,2,3,4,5]
+list_b=[7,2,6,9,7,3,5]
+print(set(a for a in list_a if a in list_b))
